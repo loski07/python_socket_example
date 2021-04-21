34ae9722abaf81302047a17a45fceee17f719d52
Pablo Diaz <pablo.diaz-fernandez@allianz.com>
2021-04-22 01:08:41 +0200

    Create test execution

---
+++ tests/integration_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/opt/software/runtime/python3.9-venv/bin/python ../server/server.py &
+/opt/software/runtime/python3.9-venv/bin/python ../client/client.py &
+/opt/software/runtime/python3.9-venv/bin/python ../client/client.py &
+#/opt/software/runtime/python3.9-venv/bin/python ../client/client.py &
+#/opt/software/runtime/python3.9-venv/bin/python ../client/client.py &
+#/opt/software/runtime/python3.9-venv/bin/python ../client/client.py &
